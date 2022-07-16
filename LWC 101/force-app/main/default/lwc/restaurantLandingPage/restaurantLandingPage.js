85baabeb85cf8dbf2ae2961889d876d64c90ae98
kate <katejohn967@gmail.com>
2022-07-16 07:58:26 +0100

    navigation service

---
+++ LWC 101/force-app/main/default/lwc/restaurantLandingPage/restaurantLandingPage.js
@@ -0,0 +1,6 @@
+import { LightningElement, track } from 'lwc';
+
+export default class RestaurantLandingPage extends LightningElement {
+    @track restaurantLogo = 'https://www.example.com/restaurant-logo.png';
+    @track restaurantDescription = 'description'
+}
