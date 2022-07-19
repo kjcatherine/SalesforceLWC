f4c35085b8de950e95ec383f299b14aa493c7187
kate <katejohn967@gmail.com>
2022-07-19 10:21:44 +0100

    navigating to new record with default values

--- LWC 101/force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
+++ LWC 101/force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
@@ -1,5 +1,7 @@
 import { LightningElement } from 'lwc';
 import {NavigationMixin} from 'lightning/navigation'
+//we need this for the default values part
+import {encodeDefaultFieldvalues} from 'lightning/pageReferenceUtils'
 export default class NavigateToObjectPage extends NavigationMixin(LightningElement) {
 
     navigateToNewRecord(){
@@ -8,6 +10,24 @@ export default class NavigateToObjectPage extends NavigationMixin(LightningEleme
             attributes: {
                 objectApiName: 'Contact',
                 actionName: 'new'
+            }
+        })
+    }
+
+    navigateToNewRecordWithDefault(){
+        const defaultValue = encodeDefaultFieldvalues({
+            FirstName: 'Raymond',
+            LastName: 'Reddington',
+            LeadSource: 'other'
+        })
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'new'
+            },
+            state: {
+                defaultFieldValues: defaultValue
             }
         })
     }
