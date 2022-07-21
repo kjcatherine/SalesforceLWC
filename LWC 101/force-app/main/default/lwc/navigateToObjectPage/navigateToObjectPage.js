0d0ace2aa40944eb265d46afad0a26b03cd00c88
kate <katejohn967@gmail.com>
2022-07-21 10:32:00 +0100

    navigate to list view page

--- LWC 101/force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
+++ LWC 101/force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
@@ -32,4 +32,17 @@ export default class NavigateToObjectPage extends NavigationMixin(LightningEleme
         })
     }
 
+    navigateToListView(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'list'
+            },
+            state:{
+                filterName: 'Recent'
+            }
+        })
+    }
+
 }
