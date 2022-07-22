a39ec31a630459c6c548892dabf187f5fabd42a0
kate <katejohn967@gmail.com>
2022-07-22 10:36:48 +0100

    Navigate to files

--- LWC 101/force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
+++ LWC 101/force-app/main/default/lwc/navigateToObjectPage/navigateToObjectPage.js
@@ -45,4 +45,14 @@ export default class NavigateToObjectPage extends NavigationMixin(LightningEleme
         })
     }
 
+    navigateToFiles(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'ContentDocument',
+                actionName: 'home'
+            }
+        })
+    }
+
 }
