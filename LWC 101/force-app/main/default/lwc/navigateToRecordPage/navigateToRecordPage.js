96ab34b2cecfc735b0c7a7cd78b6bfa9fd071aff
kate <katejohn967@gmail.com>
2022-07-23 11:21:02 +0100

    Navigating o record page in view mode

---
+++ LWC 101/force-app/main/default/lwc/navigateToRecordPage/navigateToRecordPage.js
@@ -0,0 +1,15 @@
+import { LightningElement } from 'lwc';
+import {NavigationMixin} from 'lightning/navigation'
+export default class NavigateToRecordPage extends NavigationMixin(LightningElement) {
+   // @api recordId
+    recordViewMode(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: '0033M00001I2MCzQAN',
+                objectApiName: 'Contact',
+                actionName: 'view'
+            }
+        })
+    }
+}
