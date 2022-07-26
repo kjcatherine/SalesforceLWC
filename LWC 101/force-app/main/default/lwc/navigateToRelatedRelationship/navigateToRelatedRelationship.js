c192283f439ef3f0c013fdb4fb0096294f4644d1
kate <katejohn967@gmail.com>
2022-07-26 12:19:39 +0100

    Navigate to external web page

--- LWC 101/force-app/main/default/lwc/navigateToRelatedRelationship/navigateToRelatedRelationship.js
+++ LWC 101/force-app/main/default/lwc/navigateToRelatedRelationship/navigateToRelatedRelationship.js
@@ -12,5 +12,4 @@ export default class NavigateToRelatedRelationship extends NavigationMixin(Light
             }
         })
     }
-    }
 }
---
+++ LWC 101/force-app/main/default/lwc/navigateToWebPage/navigateToWebPage.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+import {NavigationMixin} from 'lightning/navigation'
+export default class NavigateToWebPage extends NavigationMixin(LightningElement) {
+
+    navigateToWeb(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {
+                url: 'https://www.catherinekj.netlify.app',
+            }
+        })
+    }
+}
