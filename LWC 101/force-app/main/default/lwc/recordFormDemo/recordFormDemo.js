b5a16f6f0ae86b0490769a7630b694d5330ce30f
kate <katejohn967@gmail.com>
2022-08-01 11:18:56 +0100

    {

--- LWC 101/force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
+++ LWC 101/force-app/main/default/lwc/recordFormDemo/recordFormDemo.js
@@ -1,4 +1,6 @@
 import { LightningElement } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'
+
 import ACCOUNT_OBJECT from '@salesforce/schema/Account'
 import NAME_FIELD from '@salesforce/schema/Account.Name'
 import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue'
@@ -10,7 +12,13 @@ export default class RecordFormDemo extends LightningElement {
     objectName = ACCOUNT_OBJECT
     fieldList = [NAME_FIELD, ANNUAL_REVENUE_FIELD, TYPE_FIELD, INDUSTRY_FIELD]
 
-    successHandler(){
-        
+    successHandler(event){
+        console.log(event.detail.id)
+        const toastEvent = new ShowToastEvent({
+            title:"Account Created",
+            message: "Record ID: " + event.detail.id,
+            variant:"success"
+        })
+        this.dispatchEvent(toastEvent)
     }
 }
