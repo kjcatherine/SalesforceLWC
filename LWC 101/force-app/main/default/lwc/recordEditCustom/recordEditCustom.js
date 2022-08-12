e47544ad7ef9acacd859ceffe477e36bbee348b0
kate <katejohn967@gmail.com>
2022-08-12 14:08:14 +0100

    Custom Validation in lightning record edit form

---
+++ LWC 101/force-app/main/default/lwc/recordEditCustom/recordEditCustom.js
@@ -0,0 +1,32 @@
+import { LightningElement } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+export default class RecordEditCustom extends LightningElement {
+    objectName = ACCOUNT_OBJECT
+    inputValue = ''
+    handleChange(event){
+        this.inputValue = event.target.value
+    }
+    handleSubmit(event){
+        event.preventDefault()
+        const inputCmp = this.template.querySelector('lightning-input')
+        const value = inputCmp.value
+        if(!value.includes('Australia')){
+            inputCmp.setCustomValidity("The Account name must include Australia")
+        }else{
+            inputCmp.setCustomValidity("")
+            const fields = event.detail.fields
+            fields.Name = value
+            this.template.querySelector('lightning-record-edit-form').submit(fields)
+        }
+        inputCmp.reportValidity()
+    }
+    sucessHandler(){
+       const toastEvent = new ShowToastEvent({
+            title:"Account created",
+            message:"Record Id: " + event.detail.id,
+            variant:"success"
+        })
+        this.dispatchEvent(toastEvent)
+    }
+}
