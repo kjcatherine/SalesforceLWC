2e5b3003fb41c84eac2f75f915576c08aec74fee
kate <katejohn967@gmail.com>
2022-08-14 09:31:14 +0100

    wire service demo

---
+++ LWC 101/force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import Id from '@salesforce/user/Id'
+
+export default class WireDemoUserDetail extends LightningElement {
+    userId = Id;
+    //0053M000003XfuXQAS
+}
