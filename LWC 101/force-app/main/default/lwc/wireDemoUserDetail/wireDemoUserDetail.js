6bdcaa5541f895674f19e0b3070de42976a577a1
kate <katejohn967@gmail.com>
2022-08-15 09:43:00 +0100

    getting fields from user record using wire

--- LWC 101/force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
+++ LWC 101/force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -1,7 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import {getRecord} from 'lightning/uiRecordApi'
 import Id from '@salesforce/user/Id'
 
 export default class WireDemoUserDetail extends LightningElement {
     userId = Id;
     //0053M000003XfuXQAS
+
+    // @wire (adapter, {adapterConfig})
+    // propertyorfunction;
+
+    @wire(getRecord, {recordId:'0053M000003XfuXQAS', fields:['User.Name', 'User.Email']})
+
+    userDetailhandler(response){
+        consilelog(response)
+    }
+
 }
