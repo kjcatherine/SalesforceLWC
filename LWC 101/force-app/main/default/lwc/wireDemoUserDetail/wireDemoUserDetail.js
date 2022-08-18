a8da7f521462c16fbc2a93f4832445f310358ae4
kate <katejohn967@gmail.com>
2022-08-18 10:20:42 +0100

    {

--- LWC 101/force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
+++ LWC 101/force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -1,6 +1,10 @@
 import { LightningElement, wire } from 'lwc';
 import {getRecord} from 'lightning/uiRecordApi'
 import Id from '@salesforce/user/Id'
+//hardcoding values is not adviced, always import reference
+import NAME_FIELD from '@salesforce/schema/User.Name'
+import EMAIL_FIELD from '@salesforce/schema/User.Email'
+const fields = [NAME_FIELD, EMAIL_FIELD]
 
 export default class WireDemoUserDetail extends LightningElement {
     userId = Id;
@@ -10,8 +14,12 @@ export default class WireDemoUserDetail extends LightningElement {
     // @wire (adapter, {adapterConfig})
     // propertyorfunction;
 
-    @wire(getRecord, {recordId:'0053M000003XfuXQAS', fields:['User.Name', 'User.Email']})
+    // @wire(getRecord, {recordId:'0053M000003XfuXQAS', fields:['User.Name', 'User.Email']})
 
+    // @wire(getRecord, {recordId:'0053M000003XfuXQAS', fields:[NAME_FIELD, EMAIL_FIELD]})
+
+    //another way fields:fields, if key/value are same, just rep with one
+    @wire(getRecord, {recordId:'0053M000003XfuXQAS', fields})
    // userDetailhandler({data, error}) //some people destructure its the same
     // userDetailhandler(response){
     //     console.log(response)
@@ -26,8 +34,8 @@ export default class WireDemoUserDetail extends LightningElement {
         }
     }
 
-    //propert approach
-    @wire(getRecord, {recordId:'0053M000003XfuXQAS', fields:['User.Name', 'User.Email']})
+    //property approach
+    @wire(getRecord, {recordId:'0053M000003XfuXQAS', fields})
     userDetailProperty; //response from above immediately gets received here
 
 }
