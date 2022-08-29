758ffeeb32dc404c7880111de1cf5643fffae3d3
kate <katejohn967@gmail.com>
2022-08-29 13:06:16 +0100

    {

--- LWC 101/force-app/main/default/lwc/getPicklistValuesDemo/getPicklistValuesDemo.js
+++ LWC 101/force-app/main/default/lwc/getPicklistValuesDemo/getPicklistValuesDemo.js
@@ -1,10 +1,13 @@
 import { LightningElement, wire } from 'lwc';
 import {getObjectInfo, getPicklistValues} from 'lightning/uiObjectInfoApi'
-import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'
 import ACCOUNT_OBJECT from '@salesforce/schema/Account'
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'
+import TYPE_FIELD from '@salesforce/schema/Account.Type'
 export default class GetPicklistValuesDemo extends LightningElement {
     selectedIndustry = '';
+    selectedType = '';
     industryOptions = []
+    typeOptions = []
     //To get the recordTypeId
     @wire(getObjectInfo, {objectApiName: ACCOUNT_OBJECT})
     objectInfo
@@ -38,5 +41,21 @@ export default class GetPicklistValuesDemo extends LightningElement {
 
     handleChange(event) {
         this.selectedIndustry = event.detail.value;
+    }
+
+    //Second Picklist for Type field
+    @wire(getPicklistValues, {recordTypeId:'$objectInfo.data.defaultRecordTypeId', fieldApiName: TYPE_FIELD})
+    typePicklist({data, error}){
+        if(data){
+            console.log(data) 
+            this.typeOptions = [...this.generatePicklist(data)] //beacuse we dont want to update the arrays directly
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+
+    handleTypeChange(event) {
+        this.selectedType = event.detail.value;
     }
 }
