fb01ee6ef535811ddca59e9a20e8fda873bd6c27
kate <katejohn967@gmail.com>
2022-08-31 07:05:40 +0100

    {

--- LWC 101/force-app/main/default/lwc/getRecordDemo/getRecordDemo.js
+++ LWC 101/force-app/main/default/lwc/getRecordDemo/getRecordDemo.js
@@ -6,11 +6,17 @@ import OWNER_NAME_FIELD from '@salesforce/schema/Account.Owner.Name'
 import ANNUAL_REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue'
 
 export default class GetRecordDemo extends LightningElement {
+    name 
+    owner
+    AnnualRevenue
     @api recordId
     @wire(getRecord, {recordId: '$recordId', fields:[NAME_FIELD, OWNER_NAME_FIELD, ANNUAL_REVENUE_FIELD]})
     accountHandler({data}){
         if(data){
             console.log(data)
+            this.name = data.fields.Name.displayValue ? data.fields.Name.displayValue : data.fields.Name.value
+            this.AnnualRevenue = data.fields.AnnualRevenue.displayValue ? data.fields.AnnualRevenue.displayValue : data.fields.AnnualRevenue.value
+            this.owner = data.fields.owner.displayValue ? data.fields.owner.displayValue : data.fields.owner.value
         }
     }
 }
