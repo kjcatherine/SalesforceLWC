8ee3f774e61b63c28f8a6d69c6d831b28909f27e
kate <katejohn967@gmail.com>
2022-09-05 08:02:36 +0100

    uiListApi adapter

---
+++ LWC 101/force-app/main/default/lwc/getListUiDemo/getListUiDemo.js
@@ -0,0 +1,15 @@
+import { LightningElement, wire } from 'lwc';
+import {getListUi} from 'lightning/uiListApi'
+import CONTACT_OBJECT from '@salesforce/schema/Contact'
+
+export default class GetListUiDemo extends LightningElement {
+    @wire(getListUi, {objectApiName: CONTACT_OBJECT, listViewApiName: 'AllContacts'})
+    listViewHandler({data, error}){
+        if(data){
+            console.log(data) 
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+}
