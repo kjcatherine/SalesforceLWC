145bee37c3cec439a5abfecc57719894c0d3e5de
kate <katejohn967@gmail.com>
2022-09-07 08:27:30 +0100

    displaying data table of list view

--- LWC 101/force-app/main/default/lwc/getListUiDemo/getListUiDemo.js
+++ LWC 101/force-app/main/default/lwc/getListUiDemo/getListUiDemo.js
@@ -3,10 +3,12 @@ import {getListUi} from 'lightning/uiListApi'
 import CONTACT_OBJECT from '@salesforce/schema/Contact'
 
 export default class GetListUiDemo extends LightningElement {
+    contacts =[]
     @wire(getListUi, {objectApiName: CONTACT_OBJECT, listViewApiName: 'AllContacts'})
     listViewHandler({data, error}){
         if(data){
             console.log(data) 
+            this.contacts = data.records.records
         }
         if(error){
             console.error(error)
