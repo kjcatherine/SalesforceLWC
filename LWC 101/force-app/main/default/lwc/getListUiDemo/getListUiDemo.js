6012d0bfbe724d244630e61daa984e73d89a8c73
kate <katejohn967@gmail.com>
2022-09-09 08:44:04 +0100

    {

--- LWC 101/force-app/main/default/lwc/getListUiDemo/getListUiDemo.js
+++ LWC 101/force-app/main/default/lwc/getListUiDemo/getListUiDemo.js
@@ -1,13 +1,15 @@
 import { LightningElement, wire } from 'lwc';
 import {getListUi} from 'lightning/uiListApi'
 import CONTACT_OBJECT from '@salesforce/schema/Contact'
+import TITLE_FIELD from '@salesforce/schema/Contact.Title'
 
 export default class GetListUiDemo extends LightningElement {
     contacts =[]
     @wire(getListUi, {
         objectApiName: CONTACT_OBJECT, 
         listViewApiName: 'AllContacts',
-        pageSize:10
+        pageSize:10,
+        sortBy:TITLE_FIELD //by default it sorts by name
     })
     listViewHandler({data, error}){
         if(data){
