9a04e38e60cf1ad015beb41b25a88c92d17b5fe9
kate <katejohn967@gmail.com>
2022-09-12 09:41:25 +0100

    {

--- LWC 101/force-app/main/default/lwc/getNavItemsDemo/getNavItemsDemo.js
+++ LWC 101/force-app/main/default/lwc/getNavItemsDemo/getNavItemsDemo.js
@@ -2,6 +2,8 @@ import { LightningElement, wire } from 'lwc';
 import {getNavItems} from 'lightning/uiAppsApi'
 export default class GetNavItemsDemo extends LightningElement {
     @wire(getNavItems, {
+        //specify this to not get all the nav items, from developer name in console data
+        navItemNames:['standard-Account'], 
         pageSize:30
     })
     navItemsHandler({data}){
