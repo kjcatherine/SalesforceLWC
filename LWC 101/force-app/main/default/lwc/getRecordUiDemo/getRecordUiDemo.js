6a1196686bde3079669e3b4d09bba871ae1c9cd1
kate <katejohn967@gmail.com>
2022-09-15 10:45:17 +0100

    {

--- LWC 101/force-app/main/default/lwc/getRecordUiDemo/getRecordUiDemo.js
+++ LWC 101/force-app/main/default/lwc/getRecordUiDemo/getRecordUiDemo.js
@@ -3,10 +3,19 @@ import {getRecordUi} from 'lightning/uiRecordApi'
 
 export default class GetRecordUiDemo extends LightningElement {
     @api recordId
+    formFields =[ //fieldName can also be in double quotes
+        {fieldName:"AccountNumber", "label":"Account Number"},
+        {fieldName:"AnnualRevenue", "label":"Annual Revenue"},
+        {fieldName:"Name", "label":"Name"},
+        {fieldName:"Phone", "label":"Phone"},
+    ]
     @wire(getRecordUi, {recordIds:'$recordId', layoutTypes:'Full', modes:'Edit'})
     accountRecordUiHandler({data, error}){
         if(data){
             console.log(data)
+            this.formFields = this.formFields.map(item=>{
+                return {...item, value:data.records[this.recordId].fields[item.fieldName].value}
+            })
         }
         if(error){
             console.error(error)
