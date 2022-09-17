ed8aefdaab062720df921b93917df39125cbac39
kate <katejohn967@gmail.com>
2022-09-17 11:51:12 +0100

    {

--- LWC 101/force-app/main/default/lwc/createRecordDemo/createRecordDemo.js
+++ LWC 101/force-app/main/default/lwc/createRecordDemo/createRecordDemo.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc';
-import {getRecordUi} from 'lightning/uiRecordApi'
-export default class CreateRecordDemo extends LightningElement {}
+import {createRecord} from 'lightning/uiRecordApi'
+import CONTACT_OBJECT from '@salesforce/schema/Contact'
+import {showToastEvent} from 'lightning/platformShowToastEvent'
+export default class CreateRecordDemo extends LightningElement {
+    formFields={}
+    changeHandler(event){
+        const {name, value} = event.target
+        this.formFields[name]=value
+    }
+
+    createContact(){
+        const recordInput ={apiName:CONTACT_OBJECT, fields:this.formFields}
+        createRecord(recordInput)
+        .then(result=>{
+            this.showToast('Sucess!!!', `Contact created with Id ${result.id}`, 'sucess')
+            //Resets form
+            this.template.querySelector('createForm').reset()
+            this.formFields={}
+        }).catch(error=>{ 
+            this.showToast('Error Creating Record', error.body.message, 'error')
+        })
+    }
+    showToast(title, message, variant){
+        this.dispatchEvent(new showToastEvent({
+            title,
+            message,
+            variant:variant || 'success'
+        }))
+    }
+}
