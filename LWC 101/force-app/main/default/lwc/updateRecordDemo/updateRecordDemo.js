f33d5e83c9160c56c83bd62abd891efd28a1d327
kate <katejohn967@gmail.com>
2022-09-25 11:16:59 +0100

    {

--- LWC 101/force-app/main/default/lwc/updateRecordDemo/updateRecordDemo.js
+++ LWC 101/force-app/main/default/lwc/updateRecordDemo/updateRecordDemo.js
@@ -1,7 +1,17 @@
 import { LightningElement,wire } from 'lwc';
 import {getListUi} from 'lightning/uiListApi'
 import CONTACT_OBJECT from '@salesforce/schema/Contact'
+//Always define constants above the class or within a function
+const COLS =[
+    {label:'Id', fieldName:'Id'},
+    {label:'Name', fieldName:'Name'},
+    {label:'Title', fieldName:'Title'},
+    {label:'Phone', fieldName:'Phone', editable:true},
+    {label:'Email', fieldName:'Email', type:'email', editable:true},
+]
 export default class UpdateRecordDemo extends LightningElement {
+    contacts=[]
+    columns = COLS
     @wire(getListUi, {
         objectApiName: CONTACT_OBJECT,
         listViewApiName:'AllContacts'
@@ -9,9 +19,22 @@ export default class UpdateRecordDemo extends LightningElement {
     listViewHandler({data,error}){
         if(data){
             console.log(data)
+            this.contacts = data.records.records.map(item=>{
+                return {
+                    //"Name":item.fields.Name.Value
+                    "Id":this.getValue(item, 'Id'),
+                    "Name":this.getValue(item, 'Name'),
+                    "Title":this.getValue(item, 'Title'),
+                    "Phone":this.getValue(item, 'Phone'),
+                    "Email":this.getValue(item, 'Email'),
+                }
+            })
         }
         if(error){
             console.error(error)
         }
+    }
+    getValue(data, field){
+        return data.fields[field].value
     }
 }
