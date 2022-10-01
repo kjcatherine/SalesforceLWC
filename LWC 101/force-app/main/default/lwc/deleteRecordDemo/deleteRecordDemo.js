eaf8f6aef23ee0ca1f1871219cc4d29967a6d0ca
kate <katejohn967@gmail.com>
2022-10-01 12:11:53 +0100

    deleteRecord Function

--- LWC 101/force-app/main/default/lwc/deleteRecordDemo/deleteRecordDemo.js
+++ LWC 101/force-app/main/default/lwc/deleteRecordDemo/deleteRecordDemo.js
@@ -1,11 +1,17 @@
+import { deleteRecord } from 'lightning/uiRecordApi';
 import { LightningElement } from 'lwc';
 
 export default class DeleteRecordDemo extends LightningElement {
-    changeHandler(){
-
+    recordId
+    changeHandler(event){
+        this.recordId = event.target.value
     }
 
     deleteHandler(){
-        
+        deleteRecord(this.recordId).then((result)=>{
+            console.log('deleted successfully')
+        }).catch(error=>{
+            console.error(error)
+        })
     }
 }
