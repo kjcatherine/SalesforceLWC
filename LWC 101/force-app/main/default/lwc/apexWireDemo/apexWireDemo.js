ced8da5ea76264d661fd538dac6f0a61668aab72
kate <katejohn967@gmail.com>
2022-10-04 09:06:53 +0100

    importing apex into lwc

---
+++ LWC 101/force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+//we are ignoring the namespace  before classname because we dont have one
+import getAccountList from '@salesforce/apex/AccountController.getAccountList'
+
+export default class ApexWireDemo extends LightningElement {
+    
+}
