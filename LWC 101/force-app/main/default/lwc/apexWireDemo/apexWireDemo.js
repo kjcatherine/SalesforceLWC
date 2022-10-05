b30a20c0f8e87258087cc7e2691930da9ae63c8b
kate <katejohn967@gmail.com>
2022-10-05 09:39:57 +0100

    wire apes methods

--- LWC 101/force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
+++ LWC 101/force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
@@ -1,7 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 //we are ignoring the namespace  before classname because we dont have one
 import getAccountList from '@salesforce/apex/AccountController.getAccountList'
-
 export default class ApexWireDemo extends LightningElement {
-    
+    @wire(getAccountList)
+    accounts;
 }
