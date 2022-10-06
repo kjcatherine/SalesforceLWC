d8c0bc8cd1de2e3fff0671447265bf319c2ab2f1
kate <katejohn967@gmail.com>
2022-10-06 09:52:57 +0100

    Apex wire to function demo

--- LWC 101/force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
+++ LWC 101/force-app/main/default/lwc/apexWireDemo/apexWireDemo.js
@@ -4,4 +4,19 @@ import getAccountList from '@salesforce/apex/AccountController.getAccountList'
 export default class ApexWireDemo extends LightningElement {
     @wire(getAccountList)
     accounts;
+
+    accountList //should be on the top though
+    @wire(getAccountList)
+    accountsHandler({data, error}){
+        if(data){
+            this.accountList = data.map(item=>{
+                let newType = item.Type === 'Customer - Channel' ? 'Channel' : 
+                item.Type === 'Customer - Direct' ? 'Direct' : '-------'
+                return {...item, newType}
+            })
+        }
+        if(error){
+            console.error(error)
+        }
+    }
 }
