f892b48f35c6f96772eb35afbe70ad847557deea
kate <katejohn967@gmail.com>
2022-10-10 10:26:23 +0100

    bug fix

--- LWC 101/force-app/main/default/lwc/wireApexWithParams/wireApexWithParams.js
+++ LWC 101/force-app/main/default/lwc/wireApexWithParams/wireApexWithParams.js
@@ -2,6 +2,6 @@ import { LightningElement, wire } from 'lwc';
 import filterAccountTypeType from '@salesforce/apex/AccountController.filterAccountTypeType'
 export default class WireApexWithParams extends LightningElement {
     selectedType = ''
-    @wire(filterAccountTypeType, {type:$selectedType})
+    @wire(filterAccountTypeType, {type:'$selectedType'})
     filteredAccounts
 }
