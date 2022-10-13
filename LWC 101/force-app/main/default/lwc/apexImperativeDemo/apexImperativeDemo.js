2d4f4513e1fd569d338ed644b82e531bcb6ab391
kate <katejohn967@gmail.com>
2022-10-13 11:28:45 +0100

    bug fix

--- LWC 101/force-app/main/default/lwc/apexImperativeDemo/apexImperativeDemo.js
+++ LWC 101/force-app/main/default/lwc/apexImperativeDemo/apexImperativeDemo.js
@@ -1,5 +1,13 @@
 import { LightningElement } from 'lwc';
 import getAccountList from '@salesforce/apex/AccountController.getAccountList'
 export default class ApexImperativeDemo extends LightningElement {
-    
+    accounts
+    handleClick(){
+        getAccountList().then(result=>{
+            //this.accounts = result
+            cconsole.log(result)
+        }).catch(error=>{
+            console.error(error)
+        })
+    }
 }
