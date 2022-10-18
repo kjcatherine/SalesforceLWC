2490312b5dd8db10080f72290944f986b7297bca
kate <katejohn967@gmail.com>
2022-10-18 13:35:30 +0100

    fetching data from external source

---
+++ LWC 101/force-app/main/default/lwc/bookListingApp/bookListingApp.js
@@ -0,0 +1,19 @@
+import { LightningElement } from 'lwc';
+const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q='
+export default class BookListingApp extends LightningElement {
+    query='Man'
+    books
+    //Call api on the load of the component so we have to use connecteCallBack
+    connectedCallback(){
+        this.fetchBookData()
+    }
+    fetchBookData(){
+        fetch(BOOK_URL+this.query)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data)
+            this.books = data
+        })
+        .catch(error=>console.error(error))
+    }
+}
