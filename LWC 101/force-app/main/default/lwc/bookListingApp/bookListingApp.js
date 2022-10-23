4b1437046c94058bea3c53bd34df294cb9e3caf5
kate <katejohn967@gmail.com>
2022-10-23 14:30:50 +0100

    added search functionality

--- LWC 101/force-app/main/default/lwc/bookListingApp/bookListingApp.js
+++ LWC 101/force-app/main/default/lwc/bookListingApp/bookListingApp.js
@@ -3,6 +3,7 @@ const BOOK_URL = 'https://www.googleapis.com/books/v1/volumes?q='
 export default class BookListingApp extends LightningElement {
     query='Man'
     books
+    timer
     //Call api on the load of the component so we have to use connecteCallBack
     connectedCallback(){
         this.fetchBookData()
@@ -15,5 +16,12 @@ export default class BookListingApp extends LightningElement {
             this.books = data
         })
         .catch(error=>console.error(error))
+    }
+    fetchBooksHandler(event){
+        this.query= event.target.value
+        window.clearTimeout(this.timer)
+        this.timer = setTimeout(()=>{
+            this.fetchBookData()
+        }, 1000)
     }
 }
