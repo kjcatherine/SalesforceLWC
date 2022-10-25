072ead2c83a8dfa902a2b44deb61da5c66034a5e
kate <katejohn967@gmail.com>
2022-10-25 15:24:20 +0100

    meta config tags

--- LWC 101/force-app/main/default/lwc/lwcConfigDemo/lwcConfigDemo.js
+++ LWC 101/force-app/main/default/lwc/lwcConfigDemo/lwcConfigDemo.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class LwcConfigDemo extends LightningElement {}
+export default class LwcConfigDemo extends LightningElement {
+    @api heading
+    @api recordId
+    @api age
+    @api levels
+}
