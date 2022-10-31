5750b10af8285ee6e0a69455a3ea96a8e29eaefb
kate <katejohn967@gmail.com>
2022-10-31 10:07:15 +0100

    {

---
+++ LWC 101/force-app/main/default/lwc/mapsInLwcDemo/mapsInLwcDemo.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import getAccounts from '@salesforce/apex/MapControllerLwc.getAccounts'
+
+export default class MapsInLwcDemo extends LightningElement {
+    //To load the map we need the data, and to load te data we need an apex class
+
+}
