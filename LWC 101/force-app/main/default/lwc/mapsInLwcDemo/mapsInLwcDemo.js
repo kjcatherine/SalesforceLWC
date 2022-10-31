ef9b736085409542ac8d8bace191c7aa5448410c
kate <katejohn967@gmail.com>
2022-10-31 10:30:15 +0100

    {

--- LWC 101/force-app/main/default/lwc/mapsInLwcDemo/mapsInLwcDemo.js
+++ LWC 101/force-app/main/default/lwc/mapsInLwcDemo/mapsInLwcDemo.js
@@ -2,14 +2,33 @@ import { LightningElement,wire } from 'lwc';
 import getAccounts from '@salesforce/apex/MapControllerLwc.getAccounts'
 
 export default class MapsInLwcDemo extends LightningElement {
+    mapMarkers=[]
     //To load the map we need the data, and to load te data we need an apex class
     @wire(getAccounts)
     wireHandler({data, error}){
         if(data){
             console.log(data)
+            this.formatResponse(data)
         }
         if(error){
             console.error(error)
         }
+    }
+    formatResponse(data){
+        this.mapMarkers = data.map(item=>{
+            return {
+                location:{
+                    Street:item.BillingStreet || '',
+                    City:item.BillingCity || '',
+                    PostalCode:item.PostalCode || '',
+                    State:item.BillingState || '',
+                    Country:item.BillingCountry || '',
+                },
+                icon:'utility:salesforce1',
+                title:'item.Name',
+                value:'item.Name',
+                description:'item.Description'
+            }
+        })
     }
 }
