1f57a0dcce123dd63c0e11e649bea98394a525fb
kate <katejohn967@gmail.com>
2022-10-31 10:14:44 +0100

    importing class

--- LWC 101/force-app/main/default/lwc/mapsInLwcDemo/mapsInLwcDemo.js
+++ LWC 101/force-app/main/default/lwc/mapsInLwcDemo/mapsInLwcDemo.js
@@ -1,7 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire } from 'lwc';
 import getAccounts from '@salesforce/apex/MapControllerLwc.getAccounts'
 
 export default class MapsInLwcDemo extends LightningElement {
     //To load the map we need the data, and to load te data we need an apex class
-
+    @wire(getAccounts)
+    wireHandler({data, error}){
+        if(data){
+            console.log(data)
+        }
+        if(error){
+            console.error(error)
+        }
+    }
 }
