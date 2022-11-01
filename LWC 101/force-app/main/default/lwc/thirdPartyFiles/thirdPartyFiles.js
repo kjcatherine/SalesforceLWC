86d58cc46c153101f28aaf73adcf3ce14ad95355
kate <katejohn967@gmail.com>
2022-11-01 10:56:42 +0100

    loading third party JS libraries

---
+++ LWC 101/force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
@@ -0,0 +1,23 @@
+import { LightningElement } from 'lwc';
+import MOMENT from '@salesforce/resourceUrl/moment'
+import {loadScript} from 'lightning/platformResourceLoader'
+export default class ThirdPartyFiles extends LightningElement {
+    currentDate =''
+    //we always use this hook whenever we are loading a script or css so we can perform the ops inside
+    renderedCallback(){
+        Promise.all([
+            loadScript(this, MOMENT+'/moment/momemt.min.js')
+        ]).then(() =>{
+            //success
+            this.setDateOnScreen()
+        }).catch(error =>{
+            console.error(error)
+        })
+    }
+
+    setDateOnScreen(){
+        //copied this method from the momentjs.com site under multiple locale support, 2nd to last
+        this.currentDate = moment().format('LLLL');
+    }
+
+}
