f37b28e2d33fe7a1b4d64d3e086ab338320d6b48
kate <katejohn967@gmail.com>
2022-11-02 11:17:52 +0100

    displayed the momentjs method

--- LWC 101/force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
+++ LWC 101/force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
@@ -2,22 +2,52 @@ import { LightningElement } from 'lwc';
 import MOMENT from '@salesforce/resourceUrl/moment'
 import {loadScript} from 'lightning/platformResourceLoader'
 export default class ThirdPartyFiles extends LightningElement {
-    currentDate =''
-    //we always use this hook whenever we are loading a script or css so we can perform the ops inside
-    renderedCallback(){
-        Promise.all([
-            loadScript(this, MOMENT+'/moment/momemt.min.js')
-        ]).then(() =>{
-            //success
-            this.setDateOnScreen()
-        }).catch(error =>{
-            console.error(error)
-        })
+    currentDate=''
+    isLibLoaded = false
+    renderedCallback(){ 
+        if(this.isLibLoaded){ 
+            return
+        } else { 
+   
+            loadScript(this, MOMENT+'/moment/moment.min.js').then(()=>{ 
+                this.setDateOnScreen()
+            }).catch(error=>{ 
+                console.error(error)
+            })
+            this.isLibLoaded = true
+        }
+       
     }
-
-    setDateOnScreen(){
-        //copied this method from the momentjs.com site under multiple locale support, 2nd to last
-        this.currentDate = moment().format('LLLL');
+    setDateOnScreen(){ 
+        this.currentDate = moment().format('LLLL')
     }
-
 }
+
+
+
+
+
+
+// import { LightningElement } from 'lwc';
+// import MOMENT from '@salesforce/resourceUrl/moment'
+// import {loadScript} from 'lightning/platformResourceLoader'
+// export default class ThirdPartyFiles extends LightningElement {
+//     currentDate=''
+//     //we always use this hook whenever we are loading a script or css so we can perform the ops inside
+//     renderedCallback(){
+//         Promise.all([
+//             loadScript(this, MOMENT+'/moment/momemt.min.js')
+//         ]).then(() =>{
+//             //success
+//             this.setDateOnScreen()
+//         }).catch(error =>{
+//             console.error(error)
+//         })
+//     }
+
+//     setDateOnScreen(){
+//         //copied this method from the momentjs.com site under multiple locale support, 2nd to last
+//         this.currentDate = moment().format('LLLL');
+//     }
+
+// }
