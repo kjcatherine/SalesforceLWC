725f5ec30b0a47000858293de454f3324f93acb7
kate <katejohn967@gmail.com>
2022-11-05 12:42:10 +0100

    using content asset file in LWC

---
+++ LWC 101/force-app/main/default/lwc/contentAssetFiles/contentAssetFiles.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import CATHERINEHCHUKWU from '@salesforce/contentAssetUrl/CatherineHChukwupdf'
+
+export default class ContentAssetFiles extends LightningElement {
+    file = CATHERINEHCHUKWU
+}
