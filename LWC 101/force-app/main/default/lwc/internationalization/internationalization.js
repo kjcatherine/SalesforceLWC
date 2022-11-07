06ed9ce29d632f5a01a2bb33cd8b1f43747d721a
kate <katejohn967@gmail.com>
2022-11-07 13:23:42 +0100

    working with locale and currency

--- LWC 101/force-app/main/default/lwc/internationalization/internationalization.js
+++ LWC 101/force-app/main/default/lwc/internationalization/internationalization.js
@@ -4,9 +4,16 @@ import CURRENCY from '@salesforce/i18n/currency';
 
 export default class Internationalization extends LightningElement {
     number = 34564567.8
-    formattedNumber = new Intl.NumberFormat(LOCALE, {
-        style: 'currency',
-        currency: CURRENCY,
-        currencyDisplay: 'symbol'
-    }).format(this.number)
+
+//     formattedNumber = new Intl.NumberFormat(LOCALE, {
+//         style: 'currency',
+//         currency: CURRENCY,
+//         currencyDisplay: 'symbol'
+//     }).format(this.number)
+formattedNumber = new Intl.NumberFormat('ar-EG', {
+    style:'currency',
+    currency:USD,
+    currencyDisplay: 'symbol'
+}).format(this.number)
 }
+
