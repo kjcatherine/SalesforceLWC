7303dff07a45e1b3a05e3bc9e10b74cf250ff184
kate <katejohn967@gmail.com>
2022-11-09 13:29:32 +0100

    internationalization logic

--- LWC 101/force-app/main/default/lwc/internationalization/internationalization.js
+++ LWC 101/force-app/main/default/lwc/internationalization/internationalization.js
@@ -1,10 +1,11 @@
 import { LightningElement } from 'lwc';
 import LOCALE from '@salesforce/i18n/locale';
 import CURRENCY from '@salesforce/i18n/currency';
-
+//to get direction of the text
+import DIR from '@salesforce/i18n/dir';
 export default class Internationalization extends LightningElement {
     number = 34564567.8
-
+    dir = DIR
 //     formattedNumber = new Intl.NumberFormat(LOCALE, {
 //         style: 'currency',
 //         currency: CURRENCY,
