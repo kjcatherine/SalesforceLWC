0fcbf2b20ab8d35f3f8a54905a550b7c153d7899
kate <katejohn967@gmail.com>
2022-11-11 13:53:34 +0100

    custom labels example

--- LWC 101/force-app/main/default/lwc/customLabelsDemo/customLabelsDemo.js
+++ LWC 101/force-app/main/default/lwc/customLabelsDemo/customLabelsDemo.js
@@ -1,5 +1,13 @@
 import { LightningElement } from 'lwc';
 import DESCRIPTION_ONE from '@salesforce/label/c.descriptionOne'
+import DESCRIPTION_TWO from '@salesforce/label/c.descriptionTwo'
 export default class CustomLabelsDemo extends LightningElement {
-    descriptionOne = DESCRIPTION_ONE
+    // descriptionOne = DESCRIPTION_ONE
+    // descriptionTwo = DESCRIPTION_TWO
+
+    //listing them all one by one is not ideal so we do this
+    LABELS = {
+        descriptionOne:DESCRIPTION_ONE,
+        descriptionTwo:DESCRIPTION_TWO
+    }
 }
