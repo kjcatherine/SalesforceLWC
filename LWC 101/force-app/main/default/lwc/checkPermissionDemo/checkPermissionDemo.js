4785580dd0b9d5ef7022c8f87a4a832ed3ef68c2
kate <katejohn967@gmail.com>
2022-11-13 14:40:23 +0100

    custom permission sets

--- LWC 101/force-app/main/default/lwc/checkPermissionDemo/checkPermissionDemo.js
+++ LWC 101/force-app/main/default/lwc/checkPermissionDemo/checkPermissionDemo.js
@@ -1,8 +1,12 @@
 import { LightningElement } from 'lwc';
 import hasViewAllData from '@salesforce/userPermission/ViewAllData'
+import myCustomPermission from '@salesforce/customPermission/show_details'
 
 export default class CheckPermissionDemo extends LightningElement {
     get hasViewAllDataAvailable(){
         return hasViewAllData
+    }
+    get hasCustomPermission(){
+        return myCustomPermission 
     }
 }
