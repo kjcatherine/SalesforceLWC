f315ef5320e05ec1b32341d604e90d67c988b679
kate <katejohn967@gmail.com>
2022-11-20 16:28:44 +0100

    showing info and warning notifications

--- LWC 101/force-app/main/default/lwc/notifications/notifications.js
+++ LWC 101/force-app/main/default/lwc/notifications/notifications.js
@@ -2,20 +2,39 @@ import { LightningElement } from 'lwc';
 import {ShowToastEvent} from'lightning/platformShowToastEvent'
 export default class Notifications extends LightningElement {
     toastHandler(){
-      const evt = new ShowToastEvent({
-            title:'Success!!',
-            message:'Account Created',
-            variant:'success'
-        })
-        this.dispatchEvent(evt)
+        this.showToast('Success!!', 'Account Created','success')
+    //   const evt = new ShowToastEvent({
+    //         title:'Success!!',
+    //         message:'Account Created',
+    //         variant:'success'
+    //     })
+    //     this.dispatchEvent(evt)
     }
 toastHandlerTwo(){
+    this.showToast('Error!!', 'Account Creation failed','error')
+    // const evt = new ShowToastEvent({
+    //     title:'Error!!',
+    //     message:'Account Creation failed',
+    //     variant:'error'
+    // })
+    // this.dispatchEvent(evt)
+}
+
+toastHandlerThree(){
+    this.showToast('Warning!!', 'Password should have alphanumeric charcters!','warning')
+}
+
+toastHandlerFour(){
+    this.showToast('Info!!', 'Summer 22 is available!','info')
+}
+ 
+//to make this more efficient, create a common method
+showToast(title, message, variant){
     const evt = new ShowToastEvent({
-        title:'Error!!',
-        message:'Account Creation failed',
-        variant:'error'
+        title,
+        message,
+        variant
     })
     this.dispatchEvent(evt)
 }
-    
 }
