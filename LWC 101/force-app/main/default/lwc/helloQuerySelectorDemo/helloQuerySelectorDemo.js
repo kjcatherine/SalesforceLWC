c9fc5e742486caf8c1f01a2a04df9083ad155e7e
kate <katejohn967@gmail.com>
2022-12-27 11:20:01 +0100

    added an array and a loop

--- LWC 101/force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
+++ LWC 101/force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -1,9 +1,16 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloQuerySelectorDemo extends LightningElement {
+    userNames =["John", "James", "Hayley", "Mary"]
     fetchDetailHandler(){
         //saving into a variable is a choice
         const elem = this.template.querySelector('h1');
+        //querying the divs
+        const userElements = this.template.querySelectorAll(".name")
+        //userElements is not exactly an array so convert with Array.from before looping over it
+        Array.from(userElements).forEach(item =>{
+            console.log(item.innerText)
+        })
         console.log(elem.innerText)
     }
 }
