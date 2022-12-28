c51c6178d9f3514c33b4f93f922f13845a071a80
kate <katejohn967@gmail.com>
2022-12-28 12:19:17 +0100

    added array and for each loop in helloQuerySelectorDemo.js file

--- LWC 101/force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
+++ LWC 101/force-app/main/default/lwc/helloQuerySelectorDemo/helloQuerySelectorDemo.js
@@ -5,12 +5,15 @@ export default class HelloQuerySelectorDemo extends LightningElement {
     fetchDetailHandler(){
         //saving into a variable is a choice
         const elem = this.template.querySelector('h1');
+        elem.style.border="1px solid red";
+        console.log(elem.innerText)
+
         //querying the divs
         const userElements = this.template.querySelectorAll(".name")
         //userElements is not exactly an array so convert with Array.from before looping over it
         Array.from(userElements).forEach(item =>{
             console.log(item.innerText)
+            item.setAttribute("title", item.innerText)
         })
-        console.log(elem.innerText)
     }
 }
