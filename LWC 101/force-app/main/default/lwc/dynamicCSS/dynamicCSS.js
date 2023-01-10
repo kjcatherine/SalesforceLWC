45eaccb8f04c4807aef664886f10f7c426f47afe
kate <katejohn967@gmail.com>
2023-01-10 11:04:27 +0100

    added dynamic css

--- LWC 101/force-app/main/default/lwc/dynamicCSS/dynamicCSS.js
+++ LWC 101/force-app/main/default/lwc/dynamicCSS/dynamicCSS.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class DynamicCSS extends LightningElement {}
+export default class DynamicCSS extends LightningElement {
+    percent = 10;
+    changeHandler(event){
+    this.percent = event.target.value;
+    }
+
+    get percentage(){
+        return `width:${this.percent}%`
+    }
+}
