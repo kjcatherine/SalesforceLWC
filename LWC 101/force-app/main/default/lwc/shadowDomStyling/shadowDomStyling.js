a830decb49e837d95c26e98cccfd5de430e84cf4
kate <katejohn967@gmail.com>
2023-01-12 11:59:23 +0100

    appended style to the shadow dom button

--- LWC 101/force-app/main/default/lwc/shadowDomStyling/shadowDomStyling.js
+++ LWC 101/force-app/main/default/lwc/shadowDomStyling/shadowDomStyling.js
@@ -1,3 +1,15 @@
 import { LightningElement } from 'lwc';
     
-export default class ShadowDomStyling extends LightningElement {}
+export default class ShadowDomStyling extends LightningElement {
+    renderedCallback(){
+        const style = document.createElement('style');
+        //this above generates a <style></style> behind the scenes
+        style.innerText = `c-shadow-dom-styling .slds-button{
+            background: red;
+            color: white;
+        }`
+        this.template.querySelector('lightning-button').appendChild(style)
+        //we copied the .slds button from the shadow dom in inspect
+
+    }
+}
