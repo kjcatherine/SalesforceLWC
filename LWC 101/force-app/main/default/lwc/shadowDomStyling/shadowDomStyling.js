bb18555a9701efb1fe98f1f7be2f58a9e9cffec8
kate <katejohn967@gmail.com>
2023-01-13 12:11:57 +0100

    added isLoaded condition and removed the css file to demostrate that shadow dom styling worked

--- LWC 101/force-app/main/default/lwc/shadowDomStyling/shadowDomStyling.js
+++ LWC 101/force-app/main/default/lwc/shadowDomStyling/shadowDomStyling.js
@@ -1,15 +1,19 @@
 import { LightningElement } from 'lwc';
     
 export default class ShadowDomStyling extends LightningElement {
+    isLoaded =false; //means dont run the following code
     renderedCallback(){
+        if(this.isLoaded) return
         const style = document.createElement('style');
         //this above generates a <style></style> behind the scenes
         style.innerText = `c-shadow-dom-styling .slds-button{
             background: red;
             color: white;
         }`
-        this.template.querySelector('lightning-button').appendChild(style)
+        this.template.querySelector('lightning-button').appendChild(style);
         //we copied the .slds button from the shadow dom in inspect
+        //if isLoaded is false then run them
+        this.isLoaded = true;
 
     }
 }
