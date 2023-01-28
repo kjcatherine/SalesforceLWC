385393612254e18f63ac4807b35722775a7b9738
kate <katejohn967@gmail.com>
2023-01-28 08:56:48 +0100

    Parent to child communication for non-primitive data types

--- LWC 101/force-app/main/default/lwc/p2cAlertComponent/p2cAlertComponent.js
+++ LWC 101/force-app/main/default/lwc/p2cAlertComponent/p2cAlertComponent.js
@@ -1,8 +1,8 @@
 import { LightningElement, api } from 'lwc';
 
 export default class P2cAlertComponent extends LightningElement {
-    @api message;
-    @api cardHeading;
-    @api number;
-    @api isValid;
+    @api message
+    @api cardHeading
+    @api number
+    @api isValid
 }
---
+++ LWC 101/force-app/main/default/lwc/p2cCarouselComponent/p2cCarouselComponent.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class P2cCarouselComponent extends LightningElement {}
