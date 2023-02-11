23195135c5b2bcb21a52565d4db8103257f64e02
kate <katejohn967@gmail.com>
2023-02-11 16:01:55 +0100

    getter and setter method in lwc

--- LWC 101/force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
+++ LWC 101/force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
@@ -1,5 +1,14 @@
 import { LightningElement, api } from 'lwc';
 
 export default class SetterDemoChild extends LightningElement {
-    @api detail
+   // @api detail
+   // for the use case where we want the child to show the component
+   userDetail;
+   @api
+   get detail(){
+    return this.userDetail
+   }
+   set detail(data){
+        this.userDetail = data
+   }
 }
