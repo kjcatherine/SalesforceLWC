f913c7799ea7947500c07dfd6cb69db0bab73c16
kate <katejohn967@gmail.com>
2023-02-13 16:09:37 +0100

    fixing the setter error by making a copy of the object

--- LWC 101/force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
+++ LWC 101/force-app/main/default/lwc/setterDemoChild/setterDemoChild.js
@@ -9,7 +9,7 @@ export default class SetterDemoChild extends LightningElement {
     return this.userDetail
    }
    set detail(data){
-        data.age = data.age*2
-        this.userDetail = data
+        let newAge = data.age*2
+        this.userDetail = {...data, age:newAge}
    }
 }
