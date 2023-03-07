6e18bdd9cd6dd1966211fe6e8429230772b504cd
kate <katejohn967@gmail.com>
2023-03-07 20:03:01 +0100

    handling footer class line

--- LWC 101/force-app/main/default/lwc/customLightningCard/customLightningCard.js
+++ LWC 101/force-app/main/default/lwc/customLightningCard/customLightningCard.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class CustomLightningCard extends LightningElement {}
+export default class CustomLightningCard extends LightningElement {
+    //this handles the line in the footer
+    handleSlotFooterChange(){
+        const footerElem = this.template.querySelector('footer')
+        if(footerElem){
+        footerElem.classList.remove('slds-hide')
+        }
+    }
+}
