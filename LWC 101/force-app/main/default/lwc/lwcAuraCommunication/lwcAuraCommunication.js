8a9aa3e5f3e423a8051263440c9d350967adfa44
kate <katejohn967@gmail.com>
2023-03-09 20:58:33 +0100

    passing lwc attribute into aura

--- LWC 101/force-app/main/default/lwc/lwcAuraCommunication/lwcAuraCommunication.js
+++ LWC 101/force-app/main/default/lwc/lwcAuraCommunication/lwcAuraCommunication.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class LwcAuraCommunication extends LightningElement {}
+export default class LwcAuraCommunication extends LightningElement {
+    @api title;
+}
