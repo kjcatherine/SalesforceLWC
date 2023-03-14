5dfc1edce9d3c9c5dc66eb4602f2c2193d6a4866
kate <katejohn967@gmail.com>
2023-03-14 11:48:34 +0100

    created the recieving component

--- LWC 101/force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
+++ LWC 101/force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
+import SAMPLEMC from "@salesforce/messageChannel/SampleMessageChannel__c"
+//to get the receive the data that is published by comp A
+import { subscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
+export default class LmsComponentX extends LightningElement {
+    receivedMessage;
+    @wire(MessageContext)
+    context;
 
-export default class LmsComponentX extends LightningElement {}
+    connectedCallback(){
+        this.subscribeMessage()
+    }
+    // subscribe(messageContext, messageChannel, listener, subscriberOptions)
+    subscribeMessage(){
+        subscribe(this.context, SAMPLEMC, (message)=>{this.handleMessage(message)}, {scope:APPLICATION_SCOPE})
+    }
+    handleMessage(message){
+        this.receivedMessage = message.lmsData.value ? message.lmsData.value : "No Message Published"
+    }
+
+    
+}
