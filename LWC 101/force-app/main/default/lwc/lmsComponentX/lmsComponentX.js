b18e042610a0af5019cf736ca8e420b456b2b4ca
kate <katejohn967@gmail.com>
2023-03-16 12:06:06 +0100

    unsubscribing to an event using LMS

--- LWC 101/force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
+++ LWC 101/force-app/main/default/lwc/lmsComponentX/lmsComponentX.js
@@ -1,9 +1,10 @@
 import { LightningElement, wire } from 'lwc';
 import SAMPLEMC from "@salesforce/messageChannel/SampleMessageChannel__c"
 //to get the receive the data that is published by comp A
-import { subscribe, MessageContext, APPLICATION_SCOPE } from 'lightning/messageService';
+import { subscribe, MessageContext, APPLICATION_SCOPE, unsubscribe } from 'lightning/messageService';
 export default class LmsComponentX extends LightningElement {
     receivedMessage;
+    subscription;
     @wire(MessageContext)
     context;
 
@@ -12,11 +13,14 @@ export default class LmsComponentX extends LightningElement {
     }
     // subscribe(messageContext, messageChannel, listener, subscriberOptions)
     subscribeMessage(){
-        subscribe(this.context, SAMPLEMC, (message)=>{this.handleMessage(message)}, {scope:APPLICATION_SCOPE})
+        this.subscription = subscribe(this.context, SAMPLEMC, (message)=>{this.handleMessage(message)}, {scope:APPLICATION_SCOPE})
     }
     handleMessage(message){
         this.receivedMessage = message.lmsData.value ? message.lmsData.value : "No Message Published"
     }
-
+    unsubscribeMessage(){
+        unsubscribe(this.subscription)
+        this.subscription = null
+    }
     
 }
