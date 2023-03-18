0eaf7b500f28c39e71156803638bfc9854692a39
kate <katejohn967@gmail.com>
2023-03-18 13:49:45 +0100

    publishing a message from aura and listning to it from LWC using LMS

--- LWC 101/force-app/main/default/aura/lmsAuraComponent/lmsAuraComponentController.js
+++ LWC 101/force-app/main/default/aura/lmsAuraComponent/lmsAuraComponentController.js
@@ -1,13 +1,15 @@
 ({
     handleMessage : function(component, message) {
-
+        if(message != null && message.getParam("lmsData") !=null){
+            component.set("v.messageRecieved", message.getParam("lmsData").value)
+        }
     },
     inputHandler : function(component, event){
         console.log(event.target.value)
         component.set("v.messageValue", event.target.value)
     },
     publishMessage: function(component){
-        let msg = component.get("v,messageValue")
+        let msg = component.get("v.messageValue")
         let message = {
             lmsData:{
                 value:msg
