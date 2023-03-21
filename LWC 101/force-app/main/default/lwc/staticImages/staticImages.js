4d7ef5ad8a3405ebdcc56c0db29601aa49993d4f
kate <katejohn967@gmail.com>
2023-03-21 15:38:04 +0100

    loading static images into LWC

---
+++ LWC 101/force-app/main/default/lwc/staticImages/staticImages.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import USER_IMAGE from '@salesforce/resourceUrl/user_image'
+
+export default class StaticImages extends LightningElement {
+    userImage = USER_IMAGE
+}
