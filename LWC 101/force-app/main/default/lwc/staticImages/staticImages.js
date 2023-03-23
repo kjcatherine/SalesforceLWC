003a5bcb4bb5b4088e0616a9d96c1e17661bf740
kate <katejohn967@gmail.com>
2023-03-23 15:47:34 +0100

    importing svg static resource

--- LWC 101/force-app/main/default/lwc/staticImages/staticImages.js
+++ LWC 101/force-app/main/default/lwc/staticImages/staticImages.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
 import USER_IMAGE from '@salesforce/resourceUrl/user_image'
+import USER_SVG from '@salesforce/resourceUrl/user_svg'
 
 export default class StaticImages extends LightningElement {
     userImage = USER_IMAGE
+    userSvg = USER_SVG
 }
