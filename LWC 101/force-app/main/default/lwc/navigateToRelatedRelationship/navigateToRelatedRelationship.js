9cedb42621abde6ade88d0d62eac43f02aae32ab
kate <katejohn967@gmail.com>
2023-04-05 11:45:37 +0100

    Navigating to relationship page

---
+++ LWC 101/force-app/main/default/lwc/navigateToRelatedRelationship/navigateToRelatedRelationship.js
@@ -0,0 +1,16 @@
+import { LightningElement } from 'lwc';
+import {NavigationMixin} from 'lightning/navigation'
+export default class NavigateToRelatedRelationship extends NavigationMixin(LightningElement) {
+    navigateTorelatedList(){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordRelationshipPage',
+            attributes: {
+                recordId: '0013M00001M7U5QQAV', //of the account object
+                objectApiName: 'Quiz_App',
+                relationshipApiName: 'Contacts',
+                actionName: 'view'
+            }
+        })
+    }
+    }
+}
