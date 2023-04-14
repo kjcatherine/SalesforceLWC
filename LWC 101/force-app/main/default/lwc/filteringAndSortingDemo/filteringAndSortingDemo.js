05908cf3970d0693cbe50399c2f4baec8ce8a38e
kate <katejohn967@gmail.com>
2023-04-14 11:22:00 +0100

    displaying data in table

--- LWC 101/force-app/main/default/lwc/filteringAndSortingDemo/filteringAndSortingDemo.js
+++ LWC 101/force-app/main/default/lwc/filteringAndSortingDemo/filteringAndSortingDemo.js
@@ -1,10 +1,15 @@
 import { LightningElement, wire } from 'lwc';
 import getContactList from '@salesforce/apex/ContactController.getContactList'
 export default class FilteringAndSortingDemo extends LightningElement {
+    headings=["Id", "Name", "Title", "Email"]
+    fullTableData=[]
+    filteredData =[]
     @wire(getContactList)
     contactHandler({data, error}){
         if(data){
             console.log(data)
+            this.fullTableData = data
+            this.filteredData = data
         }
         if(error){
             console.error(error)
