6bb7d29693813565b217c89ee289fdefbdf502b0
kate <katejohn967@gmail.com>
2023-04-28 09:01:21 +0100

    implementing pagination

--- LWC 101/force-app/main/default/lwc/getListUiDemo/getListUiDemo.js
+++ LWC 101/force-app/main/default/lwc/getListUiDemo/getListUiDemo.js
@@ -5,19 +5,32 @@ import TITLE_FIELD from '@salesforce/schema/Contact.Title'
 
 export default class GetListUiDemo extends LightningElement {
     contacts =[]
+    pageToken = null
+    nextPageToken = null
+    previousPageToken = null
     @wire(getListUi, {
         objectApiName: CONTACT_OBJECT, 
         listViewApiName: 'AllContacts',
         pageSize:10,
-        sortBy:TITLE_FIELD //by default it sorts by name
+        sortBy:TITLE_FIELD, //by default it sorts by name
+        pageToken:'$pageToken'
     })
     listViewHandler({data, error}){
         if(data){
             console.log(data) 
             this.contacts = data.records.records
+            this.nextPageToken = data.records.nextPageToken 
+            this.previousPageToken = data.records.previousPageToken 
         }
         if(error){
             console.error(error)
         }
+    }
+    handlePreviousPage(){
+        this.pageToken = this.previousPageToken
+    }
+
+    handleNextPage(){
+        this.pageToken = this.nextPageToken
     }
 }
