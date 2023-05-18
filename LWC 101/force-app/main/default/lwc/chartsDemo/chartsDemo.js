34919a15da6f4c599b453c252ef94c9af06f072e
kate <katejohn967@gmail.com>
2023-05-18 12:23:52 +0100

    creating apex controller for opportunity

--- LWC 101/force-app/main/default/lwc/chartsDemo/chartsDemo.js
+++ LWC 101/force-app/main/default/lwc/chartsDemo/chartsDemo.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
-
-export default class ChartsDemo extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import getOpportunities from '@salesforce/apex/OpportunityController.getOpportunities'
+export default class ChartsDemo extends LightningElement {
+    @wire(getOpportunities)
+    opportunityHandler({data, error}){
+        if(data){
+            console.log(data)
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+}
