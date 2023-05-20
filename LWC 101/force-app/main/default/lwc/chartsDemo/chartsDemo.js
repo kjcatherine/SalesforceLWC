8db7c397f0d4d20c2d3dca74d74e67263993d4bc
kate <katejohn967@gmail.com>
2023-05-20 14:25:36 +0100

    fixed display issue

--- LWC 101/force-app/main/default/lwc/chartsDemo/chartsDemo.js
+++ LWC 101/force-app/main/default/lwc/chartsDemo/chartsDemo.js
@@ -1,6 +1,30 @@
 import { LightningElement, wire } from 'lwc';
 import getOpportunities from '@salesforce/apex/OpportunityController.getOpportunities'
 export default class ChartsDemo extends LightningElement {
+    pieChartLabels=[]
+    pieChartData=[]
+    @wire(getOpportunities)
+    opportunityHandler({data, error}){
+        if(data){
+            console.log(data)
+            const result = data.reduce((json, val)=>({...json, [val.StageName]:(json[val.StageName]|0)+1}), {})
+            if(Object.keys(result).length){
+                this.pieChartLabels = Object.keys(result)
+                this.pieChartData = Object.values(result)
+            }
+        
+        }
+        if(error){
+            console.error(error)
+        }
+    }
+}
+
+
+
+/*import { LightningElement, wire } from 'lwc';
+import getOpportunities from '@salesforce/apex/OpportunityController.getOpportunities'
+export default class ChartsDemo extends LightningElement {
     pieChartLabels = []
     pieChartData = [] //we are using .lenght so we have to initialize with an empty array
 
@@ -19,4 +43,4 @@ export default class ChartsDemo extends LightningElement {
             console.error(error)
         }
     }
-}
+}*/
