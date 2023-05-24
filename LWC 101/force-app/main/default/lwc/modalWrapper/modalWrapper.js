076593b8732a37a6920b614ab7d72121416149f9
kate <katejohn967@gmail.com>
2023-05-24 12:12:34 +0100

    demonstrating the reuseability by creating different modals from the main one

--- LWC 101/force-app/main/default/lwc/modalWrapper/modalWrapper.js
+++ LWC 101/force-app/main/default/lwc/modalWrapper/modalWrapper.js
@@ -2,11 +2,18 @@ import { LightningElement } from 'lwc';
 
 export default class ModalWrapper extends LightningElement {
     isOpen = false
+    isOpenSecond = false
     openHandler(){
         console.log("clicked!!")
         this.isOpen = true
     }
+    openSecondHandler(){
+        this.isOpenSecond = true
+    }
     closeHandler(){
         this.isOpen = false
+    }
+    secondCloseHandler(){
+        this.isOpenSecond = false
     }
 }
