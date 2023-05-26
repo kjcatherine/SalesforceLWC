da7b19489cd9b911642b6857775f55328ec8169b
kate <katejohn967@gmail.com>
2023-05-26 12:40:40 +0100

    date validation logic

--- LWC 101/force-app/main/default/lwc/dateValidation/dateValidation.js
+++ LWC 101/force-app/main/default/lwc/dateValidation/dateValidation.js
@@ -3,8 +3,19 @@ import { LightningElement } from 'lwc';
 export default class DateValidation extends LightningElement {
     startDate
     endDate //note they are same as the name attribute in html
+    error
     dateHandler(event){
         const {name, value} = event.target
         this[name] = value //this["startDate"] = value
+    }
+    submitHandler(){
+        if(this.validateDate(this.startDate, this.endDate)){
+            console.log('Date is valid')
+        }else{
+            this.error = "Start Date cannot be greater than End date"
+        }    
+    }
+    validateDate(startDate, endDate){
+        return new Date(startDate).getTime() < new Date(endDate).getTime()
     }
 }
