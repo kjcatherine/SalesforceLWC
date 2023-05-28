5803d2973275dadc1264262ada8694ea7376620b
kate <katejohn967@gmail.com>
2023-05-28 13:16:54 +0100

    displaying accounts

--- LWC 101/force-app/main/default/lwc/csvDemo/csvDemo.js
+++ LWC 101/force-app/main/default/lwc/csvDemo/csvDemo.js
@@ -1,10 +1,12 @@
 import { LightningElement, wire } from 'lwc';
 import getAccounts from '@salesforce/apex/CsvController.getAccounts'
 export default class CsvDemo extends LightningElement {
+    accountData
     @wire(getAccounts)
     accountHandler({data, error}){
         if(data){
             console.log("This is data from Contoller", data)
+            this.accountData = data
         }
         if(error){
             console.error("An error has occured when retreiving account records", error)
