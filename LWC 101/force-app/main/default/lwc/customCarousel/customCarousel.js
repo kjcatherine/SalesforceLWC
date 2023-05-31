56b01231aba5608f3feb25ce93650074cb52f5ad
kate <katejohn967@gmail.com>
2023-05-31 10:29:49 +0100

    Logic to show only one image per time

--- LWC 101/force-app/main/default/lwc/customCarousel/customCarousel.js
+++ LWC 101/force-app/main/default/lwc/customCarousel/customCarousel.js
@@ -1,5 +1,29 @@
 import { LightningElement, api } from 'lwc';
 
+const CARD_VISIBLE_CLASSES = 'slds-show'
+const CARD_HIDDEN_CLASSES = 'slds-hide'
+
 export default class CustomCarousel extends LightningElement {
-    @api slidesData
+   slides=[]
+    // @api slidesData
+   @api
+   get slidesData(){
+    return this.slides
+   }
+
+   set slidesData(data){
+    this.slides = data.map((item,index)=>{
+        return index === 0 ? {
+            ...item,
+            slideIndex: index+1,
+            cardClasses: CARD_VISIBLE_CLASSES
+        } : {
+            ...item,
+            slideIndex: index+1,
+            cardClasses: CARD_HIDDEN_CLASSES
+        }
+    })
+   }
+
+
 }
