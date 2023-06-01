2d3423cb0d8935e1444e9a2e3dce3bb9952a46ed
kate <katejohn967@gmail.com>
2023-06-01 11:25:00 +0100

    Building the structure

--- LWC 101/force-app/main/default/lwc/customCarouselWrapper/customCarouselWrapper.js
+++ LWC 101/force-app/main/default/lwc/customCarouselWrapper/customCarouselWrapper.js
@@ -3,13 +3,19 @@ import CAROUSEL_IMAGES from '@salesforce/resourceUrl/carousel'
 export default class CustomCarouselWrapper extends LightningElement {
     slides = [
         {
-            image:`${CAROUSEL_IMAGES}/carousel/photo1.jpg`
+            image:`${CAROUSEL_IMAGES}/carousel/photo1.jpg`,
+            heading:'Caption one',
+            description:'Descripton of the first Carousel comes here'
         },
         {
-            image:`${CAROUSEL_IMAGES}/carousel/photo2.jpg`
+            image:`${CAROUSEL_IMAGES}/carousel/photo2.jpg`,
+            heading:'Caption two',
+            description:'Descripton of the second Carousel comes here'
         },
         {
-            image:`${CAROUSEL_IMAGES}/carousel/photo3.jpg`
+            image:`${CAROUSEL_IMAGES}/carousel/photo3.jpg`,
+            heading:'Caption three',
+            description:'Descripton of the third Carousel comes here'
         }
     ]
 }
