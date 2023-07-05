9dbdbe55ae357d48aa86bea8155c4790b29df7e1
kate <katejohn967@gmail.com>
2023-07-05 13:29:07 +0100

    Implementing password input with visibility toggled on and off

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/inputIcon/inputIcon.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/inputIcon/inputIcon.js
@@ -3,11 +3,25 @@ import {loadStyle} from 'lightning/platformResourceLoader'
 import input from '@salesforce/resourceUrl/input'
 
 export default class InputIcon extends LightningElement {
+    //Using the eye icon to hide and show password
+    showPassword = false
+    get passwordIcon(){
+        return this.showPassword ? 'utility:hide':'utility:preview'
+    }
+
+    get passwordType(){
+        return this.showPassword ? 'text':'password'
+    }
+
     connectedCallback(){
         loadStyle(this, input).then(()=>{
             console.log("Styles loaded succesully")
         }).catch(error=>{
             console.error(error)
         })
+    }
+//Toggling the visibility on and off
+    passwordHandler(){
+        this.showPassword = !this.showPassword
     }
 }
