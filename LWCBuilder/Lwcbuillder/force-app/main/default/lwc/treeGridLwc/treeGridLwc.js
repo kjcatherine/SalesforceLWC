1cc6e5c811e6ff3f5aec040e6f18a6a59ec5a657
kate <katejohn967@gmail.com>
2023-07-07 14:44:29 +0100

    fixed bug

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/treeGridLwc/treeGridLwc.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/treeGridLwc/treeGridLwc.js
@@ -2,13 +2,61 @@ import { LightningElement, wire } from 'lwc';
 import allAccountsWithContact from '@salesforce/apex/AccountContact.allAccountsWithContact'
 
 export default class TreeGridLwc extends LightningElement {
+    gridData = []
     @wire(allAccountsWithContact)
     accountsWithContactsResult({data, error}){
         if(data){
             console.log(data)
+            this.formatGridData(data)
         }
         if(error){
             console.error(error)
         }
+    }
+
+    //Columns
+    gridColumns=[
+        {
+            label:'Name',
+            fieldName:'Name',
+            type:'text'
+        },
+        {
+            label:'Phone',
+            fieldName:'Phone',
+            type:'text'
+        },
+        {
+            label:'Account Website',
+            fieldName:'Website',
+            type:'url',
+            typeAttributes:{
+                target:'_blank'
+            }
+        },
+    ]
+//Example of child of contact
+    dummyData = [
+        {
+            Name: 'Salesforce',
+            Email: 'Salesforce@gmail.com',
+            Website: 'salesforcetroop.com'
+        },
+        {
+            Name: 'Troop',
+            Email: 'troop@gmail.com',
+            Website: 'salesforcetroop.com'
+        }
+    ]
+
+    formatGridData(result){
+        this.gridData = result.map(item=>{
+            const {Contacts, ...accounts} = item
+            const updatedContact = Contacts.map(cont=>{
+                return {...cont, "_children":this.dummyData}
+            })
+            return {...accounts, "_children":updatedContact}
+        })
+        console.log(this.gridData)
     }
 }
