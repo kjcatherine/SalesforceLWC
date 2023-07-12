523045aa55bdc58cf237662705e0d7a28fe68f56
kate <katejohn967@gmail.com>
2023-07-12 18:45:24 +0100

    logic for getRelatedListCount wire adapter

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/getRelatedListCountDemo/getRelatedListCountDemo.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/getRelatedListCountDemo/getRelatedListCountDemo.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
-
-export default class GetRelatedListCountDemo extends LightningElement {}
+import {getRelatedListCount} from 'lightning/uiRelatedListApi'
+export default class GetRelatedListCountDemo extends LightningElement {
+  relatedData
+  @wire(getRelatedListCount, {
+    parentRecordId:'0010C00000a7TFhQAM', // The ID of the parent record that you want to get related list for
+    relatedListId:'Contacts' // The API name of a related list object such as Contacts, Opportunities etc
+  })listCountHandler({error, data}){
+    if(data){
+      console.log(JSON.stringify(data))
+      this.relatedData = data
+    }
+    if(error){
+      console.error(error)
+    }
+  }
+}
