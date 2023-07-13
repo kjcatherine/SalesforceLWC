8e39aa16b2715b976f7b1110cbd334034485e9ff
kate <katejohn967@gmail.com>
2023-07-13 18:49:15 +0100

    getRelatedListRecords adapter

---
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/getRelatedListRecordDemo/getRelatedListRecordDemo.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+import {getRelatedListRecords} from 'lightning/uiRelatedListApi'
+export default class GetRelatedListRecordDemo extends LightningElement {
+  recordList
+  @wire(getRelatedListRecords, {
+    parentRecordId:'0010C00000a7TFhQAM',
+    relatedListId:'Contacts',
+    fields:['Contact.Name', 'Contact.Id'] //optional field
+  })listRecordsHandler({data,error}){
+    if(data){
+      console.log(JSON.stringify(data))
+      this.recordList = data.records
+    }
+    if(error){
+      console.error(error)
+    }
+  }
+}
