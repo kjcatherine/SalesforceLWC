08214f9917482ad1ac5b629f0882627b3c336785
kate <katejohn967@gmail.com>
2023-07-16 07:28:05 +0100

    creating a basic LightningAlert logic

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningAlertDemo/lightningAlertDemo.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningAlertDemo/lightningAlertDemo.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 import LightningAlert from 'lightning/alert'
-export default class LightningAlertDemo extends LightningElement {}
+export default class LightningAlertDemo extends LightningElement {
+    alertHandler(){
+        //window.alert("Hello") //Deprecated in JS soon
+        LightningAlert.open({
+            message:"This is an alert"
+        })
+    }
+}
