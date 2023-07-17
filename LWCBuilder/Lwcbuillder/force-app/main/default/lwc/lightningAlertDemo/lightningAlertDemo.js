3a47dd60fcc251e73dcc7a15393f54bd34f9c316
kate <katejohn967@gmail.com>
2023-07-17 07:31:59 +0100

    trying out the other properties of LightningAlert

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningAlertDemo/lightningAlertDemo.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningAlertDemo/lightningAlertDemo.js
@@ -4,7 +4,9 @@ export default class LightningAlertDemo extends LightningElement {
     alertHandler(){
         //window.alert("Hello") //Deprecated in JS soon
         LightningAlert.open({
-            message:"This is an alert"
+            message:"This is an alert",
+            label:"Alert header",
+            theme:"success" //success -> green, warning->orange, error->red, infro->grey
         })
     }
 }
