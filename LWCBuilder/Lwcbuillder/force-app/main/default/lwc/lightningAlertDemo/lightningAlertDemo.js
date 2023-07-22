4a815f761b2277feaf51aebcd64f59f19ab99091
kate <katejohn967@gmail.com>
2023-07-22 08:07:13 +0100

    headerless variant

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningAlertDemo/lightningAlertDemo.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningAlertDemo/lightningAlertDemo.js
@@ -17,6 +17,7 @@ export default class LightningAlertDemo extends LightningElement {
         //window.alert("Hello") //Deprecated in JS soon
         await LightningAlert.open({
             message:"This is an alert",
+            variant:"headerless", //hides alert headers
             label:`I am ${name} alert header`,
             theme:name //success -> green, warning->orange, error->red, infro->grey
         }) //.then(result=>{..code})
@@ -28,4 +29,5 @@ export default class LightningAlertDemo extends LightningElement {
     add(a,b){
         console.log(a+b)
     }
+    //If you only need to show alert without any further action the n async is not needed
 }
