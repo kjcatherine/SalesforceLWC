24c8e2a993a505e24c58194a52afb05f027a98b0
kate <katejohn967@gmail.com>
2023-07-25 08:35:55 +0100

    adding refresh logic to JS

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningConfirmDemo/lightningConfirmDemo.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningConfirmDemo/lightningConfirmDemo.js
@@ -1,9 +1,17 @@
 import { LightningElement } from 'lwc';
 import LightningConfirm from 'lightning/confirm'
 export default class LightningConfirmDemo extends LightningElement {
-    confirmHandler(){
-        LightningConfirm.open({
-            message:"Would you like to refresh the page"
+    result
+    async confirmHandler(){
+        this.result = await LightningConfirm.open({
+            message:"Would you like to refresh the page",
+            label:"Are you sure?",
+            theme:"success",
+            //variant:"headerless"
         })
+        console.log(this.result)
+        if(this.result){
+            location.reload()
+        }
     }
 }
