3f7efc67941a90c6ae381c8712858f131adf6001
kate <katejohn967@gmail.com>
2023-07-27 08:03:21 +0100

    Voting eligibility use case

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningPromptDemo/lightningPromptDemo.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningPromptDemo/lightningPromptDemo.js
@@ -2,16 +2,28 @@ import { LightningElement } from 'lwc';
 import LightningPrompt from 'lightning/prompt'
 export default class LightningPromptDemo extends LightningElement {
     result
-    promptHandler(){
+    async promptHandler(){
             this.result = await LightningPrompt.open({
                 message:"Please enter your age",
-                label:"Are you sure?",
-                theme:"warning",
+                // label:"Are you sure?",
+                // theme:"warning",
                 //variant:"headerless"
             })
             console.log(this.result)
-            if(this.result){
-                location.reload()
+            if(this.result && Number(this.result) > 18){
+                console.log("Hurray you are eligible")
+            }else{
+                console.log("Sorry you are not eligible")
             }
         }
+
+
+//then method
+// promptHandler(){
+//    LightningPrompt.open({
+//         message:"Please enter your age",
+//     }).then(result=>{
+//         console.log(result)
+//     })
+    
 }
