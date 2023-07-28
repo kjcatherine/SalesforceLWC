c55a297a267241f7de0549ef7fe6cd9d59513756
kate <katejohn967@gmail.com>
2023-07-28 08:17:52 +0100

    calling a lightning alert within a lightning prompt

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningPromptDemo/lightningPromptDemo.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/lightningPromptDemo/lightningPromptDemo.js
@@ -1,22 +1,32 @@
 import { LightningElement } from 'lwc';
 import LightningPrompt from 'lightning/prompt'
+import LightningAlert from 'lightning/alert'
 export default class LightningPromptDemo extends LightningElement {
     result
-    async promptHandler(){
+    async promptHandler(){ //no need to use async since no further action required
             this.result = await LightningPrompt.open({
                 message:"Please enter your age",
-                // label:"Are you sure?",
-                // theme:"warning",
+                label:"Check you voting eligibility",
+                theme:"success",
+                defaultValue:30
                 //variant:"headerless"
             })
             console.log(this.result)
             if(this.result && Number(this.result) > 18){
-                console.log("Hurray you are eligible")
+                //Calling an alert within a prompt
+                this.alertHandler("Hurray you are eligible", "Success", "success")
             }else{
-                console.log("Sorry you are not eligible")
+                this.alertHandler("Sorry you are not eligible", "Error", "error")
             }
         }
-
+//Using alert with prompt use case
+alertHandler(message, label, theme){
+    LightningAlert.open({
+        message,
+        label,
+        theme
+    })
+}
 
 //then method
 // promptHandler(){
