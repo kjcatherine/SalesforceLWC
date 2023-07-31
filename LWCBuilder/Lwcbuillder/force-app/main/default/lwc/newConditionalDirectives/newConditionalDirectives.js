9538e7ae436fd8d8ea3b0c079ce1efd3485ad898
kate <katejohn967@gmail.com>
2023-07-31 10:26:23 +0100

    demonstrating the performance efficiency of the new directives against the old ones

--- LWCBuilder/Lwcbuillder/force-app/main/default/lwc/newConditionalDirectives/newConditionalDirectives.js
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/newConditionalDirectives/newConditionalDirectives.js
@@ -10,4 +10,29 @@ export default class NewConditionalDirectives extends LightningElement {
     showTextHandler(){
         this.showText= !this.showText
     }
+
+    //Getter demo
+    country = 'USA'
+    newCountry = 'Canada'
+    get isCountryUsa(){
+        console.log('isCountryUsa getter called')
+        return this.country === 'USA'
+    }
+    get isCountryCanada(){
+        console.log('isCountryCanada getter called')
+        return this.newCountry === 'Canada'
+    }
+
+    get isCountryUk(){
+        console.log('isCountryUk getter called')
+        return this.newCountry === 'UK'
+    }
+
+    changeHandler(event){
+        this.country = event.target.value
+    }
+
+    changeHandler1(event){
+        this.newCountry = event.target.value
+    }
 }
