40bab1f72a8bf068803a0aad37a462715477b02b
kate <katejohn967@gmail.com>
2023-08-01 10:48:02 +0100

    refs demo

---
+++ LWCBuilder/Lwcbuillder/force-app/main/default/lwc/refsDemo/refsDemo.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class RefsDemo extends LightningElement {
+    submitHandler(){
+        console.log("this.refs.nameRef", this.refs.nameRef)
+        //Equivalent to event.target
+        const nameVal = this.refs.nameRef.value
+        const ageVal = this.refs.ageRef.value
+        console.log("nameValue", nameVal)
+        console.log("ageValue", ageVal)
+    }
+}
